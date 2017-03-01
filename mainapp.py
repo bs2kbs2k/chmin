d77ecd81bf9ca1422eed50ba44e48fb08241c84d
곽대현 <bs2k@gwagdaehyeon-ui-Mac-mini.local>
2017-03-01 13:55:22 +0900

    New Init

--- mainapp.py
+++ mainapp.py
@@ -1,3 +1,11 @@
-print("Hello, 세계!")
-#대현아_이게_정상이지
-#ㅇㅇ
+import time
+print('무인도에서 살아남기\n')
+time.sleep(1)
+print('------------------\n\n')
+inventory = ['칼','비닐봉지', '줄']
+i = 'False'
+time.sleep(1)
+print('당신은 이 무인도에서 생존하게 될것이다.')
+time.sleep(1)
+print('있는 것은 "칼"과 "비닐봉지" 그리고 "줄"뿐이다')
+time.sleep(1)
