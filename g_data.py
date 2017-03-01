2eb0077c47874148b6d74baae9d75477ed6d6143
곽대현 <bs2k@gwagdaehyeon-ui-Mac-mini.local>
2017-03-01 15:26:53 +0900

    New init return

---
+++ g_data.py
@@ -0,0 +1,23 @@
+sword = 
+	{'num' : 0
+	,'name' : '칼'
+	,'use' : sword_use}
+
+bag = 
+	{'num' : 1
+	,'name' : '봉지'
+	,'use' : bag_use}
+
+	rope = 
+	{'num' : 2
+	,'name' : '줄'
+	,'use' : rope_use}
+
+def sword_use(self):
+	pass
+
+def bag_use(self):
+	pass
+
+def rope_use(self):
+	pass
--- mainapp.py
+++ mainapp.py
@@ -1,11 +1,37 @@
 import time
-print('무인도에서 살아남기\n')
-time.sleep(1)
-print('------------------\n\n')
-inventory = ['칼','비닐봉지', '줄']
-i = 'False'
-time.sleep(1)
-print('당신은 이 무인도에서 생존하게 될것이다.')
-time.sleep(1)
-print('있는 것은 "칼"과 "비닐봉지" 그리고 "줄"뿐이다')
-time.sleep(1)
+import g_data
+
+
+class item:
+
+	def __init__(self, item_type):
+		self.data = item_type
+		self.use = self.data['use']
+
+class data:
+
+	def __init__(self, inventory = 
+		[ item(g_data.sword)
+		, item(g_data.bag)
+		, item(g_data.rope) ]):
+		self.inventory = inventory
+
+class game:
+
+	def __init__(self, save_data):
+		self.inventory = save_data.inventory
+
+	def main(self):
+		print('무인도에서 살아남기')
+		print('------------------')
+		print('당신은 이 무인도에서 생존하게 될것이다.')
+		print()
+		inv_print()
+	
+	def inv_print(self):
+		print(
+			' '.join(
+				[ '있는 것은'
+				, ', '.join([ looper.data[‘name’] 
+					for looper in self.inventory ])
+				, '입니다.' ]))
